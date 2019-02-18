34baebd01560712142942e997268d683e542e391
Джабашев Нурбек Кадербекович <@corp.itstep>
2019-02-18 20:19:08 +0600

    Indexers

---
+++ Array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    class Array
+    {
+        private int[] array;
+        public int Size { get; set; }
+
+        public Array(int size)
+        {
+            Size = size;
+            array = new int[size];
+        }
+
+        public int this[int i]
+        {
+            get { return array[i]; }
+            set { array[i] = (int)Math.Pow(value, 2); }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int ARRAY_SIZE = 10;
+            Array array = new Array(ARRAY_SIZE);
+
+            Console.WriteLine("Длина массива: " + array.Size + "\n");
+
+            Random rnd = new Random();
+
+            try
+            {
+                for (int i = 0; i < array.Size; i++)
+                {
+                    array[i] = rnd.Next(1, 10);
+                    Console.WriteLine("array[{0}]: {1}", i, array[i]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
